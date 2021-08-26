7ec5aa90c4da49ac996d5362e0b329fae07d2681
Fazeela <fazeelanizam13@gmail.com>
2021-08-26 22:38:20 +0530

    href and as incompatibility issue

--- components/Card.js
+++ components/Card.js
@@ -4,7 +4,7 @@ import styles from "../styles/card.module.css";
 export default function Card({ slug, title, date }) {
   return (
     <div className={styles.root}>
-      <Link href={`/posts/${slug}`} as={`${process.env.URL}/posts/${slug}`}>
+      <Link href={`${process.env.URL}/posts/${slug}`}>
         <a style={{ width: "100%" }}>
           <p className={styles.title}>{title}</p>
           <p className={styles.date}>{date}</p>
